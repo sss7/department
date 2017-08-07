9755cb2a57dd9bca6ffd2a7a72bdd25425964461
Pavel <dym91@list.ru>
2017-08-07 13:34:22 +0300

    Initial commit

--- src/test/Person.java
+++ src/test/Person.java
@@ -2,7 +2,6 @@ package test;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Person implements Comparable<Person> {
